fcb2daecbef018cbc4d38b4c7a2a79c63116f0e4
gleb <glibkostsov@gmail.com>
2022-07-06 00:11:26 +0200

    add basic date picker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,8 +3,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
 export class MaterialModule {}
