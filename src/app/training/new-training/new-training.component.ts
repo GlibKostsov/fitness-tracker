652e6de2af8a5aff45732b0057124dcfc2172ae9
gleb <glibkostsov@gmail.com>
2022-08-01 21:29:08 +0200

    lesson 55: add trainingService & and inject data to NewTraining Component

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
+  exercises: Exercise[] = [];
   @Output() trainingStart = new EventEmitter<void>();
-  constructor() {}
+  constructor(private trainingService: TrainingService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.exercises = this.trainingService.getAvailableExercises();
+  }
 
   onStartTraining() {
     this.trainingStart.emit();
